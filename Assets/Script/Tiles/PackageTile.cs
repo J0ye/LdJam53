0674ad38edfaa58cf588ea4125d050463b40aea3
Kapuetze <joni@vocalpatrioten.de>
2023-04-29 19:23:08 +0200

    Destinations and score

---
+++ Assets/Script/Tiles/PackageTile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Tile))]
+public class PackageTile : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<Car>(out Car car))
+        {
+            // give the package to car
+            car.IncreasePackages();
+
+            // destroy the package object
+            Destroy(gameObject);
+        }
+    }
+}
