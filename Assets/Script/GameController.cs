4d0d94c6781c4a66daeebc1314709812052ff05e
Kapuetze <joni@vocalpatrioten.de>
2023-04-29 18:04:45 +0200

    Meeeehehr tiels

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -8,7 +8,15 @@ public class GameController : MonoBehaviour
     public static GameController instance;
     public Player player;
 
+    /// <summary>
+    /// The current score
+    /// </summary>
     int score = 0;
+
+    /// <summary>
+    /// Game is currently paused
+    /// </summary>
+    bool isPaused;
 
 
     private void Awake()
@@ -35,24 +43,68 @@ public class GameController : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Starts the current level
+    /// </summary>
+    public void StartLevel()
+    {
+        // Spawn the first customer
+        ResumeLevel();
+    }
+
+    /// <summary>
+    /// Restarts the scene
+    /// </summary>
     public void RestartLevel()
     {
+        ResumeLevel();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void EndLevel()
+    /// <summary>
+    /// Restarts the scene
+    /// </summary>
+    public void GoToMain()
     {
-        // TODO
+        SceneManager.LoadScene(0);
     }
 
+    /// <summary>
+    /// Pause the game
+    /// </summary>
     public void PauseLevel()
     {
-
+        Time.timeScale = 0;
+        isPaused = true;
     }
 
+    /// <summary>
+    /// Resume the game
+    /// </summary>
     public void ResumeLevel()
     {
-
+        Time.timeScale = 1;
+        isPaused = false;
     }
 
+    /// <summary>
+    /// Ends the level and shows score
+    /// </summary>
+    public void EndLevel()
+    {
+        // Game is over
+        PauseLevel();
+        UIController.instance.ShowEndScreen(score);
+    }
+
+    /// <summary>
+    /// Add score to the game score
+    /// </summary>
+    /// <param name="amount">The score amount to be added</param>
+    public void AddScore(int amount)
+    {
+        score += amount;
+
+        UIController.instance.SetScore(score);
+    }
 }
