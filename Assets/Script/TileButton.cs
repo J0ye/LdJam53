89e9b056fbf3115fd163aa044aef6929a962853c
MX-W <wendl.max@gmail.com>
2023-04-30 00:42:19 +0200

    Added images to tile buttons

--- Assets/Script/TileButton.cs
+++ Assets/Script/TileButton.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileButton : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text text;
+
+    [SerializeField]
+    private RawImage image;
 
     Tile tile;
     // Start is called before the first frame update
@@ -19,6 +23,13 @@ public class TileButton : MonoBehaviour
     {
         this.tile = tile;
         text.text = tile.name;
+
+        image.texture = tile.imageTexture;
+
+        if(this.tile.imageRotation > 0)
+        {
+            transform.Rotate(new Vector3(0.0f, 0.0f, this.tile.imageRotation));
+        }
     }
 
     public void SelectTile()
