983fc354333e441097228908c155273fdd085d92
Kapuetze <joni@vocalpatrioten.de>
2023-04-30 15:15:39 +0200

    GigaGrid

---
+++ Assets/Script/GigaGrid.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class GigaGrid : MonoBehaviour
+{
+    Dictionary<Vector3Int, Tile> tiles = new Dictionary<Vector3Int, Tile>();
+
+    Grid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    public bool GetAt(Vector3Int position)
+    {
+        Tile tile = null;
+        if (tiles.ContainsKey(position))
+            tile = tiles[position];
+        
+        return tile;
+    }
+
+    public GameObject PlaceTile(Vector3 worldPosition, GameObject tilePrefab)
+    {
+        // get the cell center in world position
+        Vector3Int position = grid.WorldToCell(worldPosition);
+        Vector3 worldPositionCenter = grid.GetCellCenterWorld(position);
+
+        var newTile = Instantiate(tilePrefab, worldPositionCenter, Quaternion.identity);
+
+        PlaceTile(position, newTile.GetComponent<Tile>());
+        return newTile;
+    }
+
+    public GameObject PlaceTile(Vector3Int position, GameObject tilePrefab)
+    {
+        // get the cell center in world position
+        Vector3 worldPosition = grid.GetCellCenterWorld(position);
+
+        var newTile = Instantiate(tilePrefab, worldPosition, Quaternion.identity);
+
+        PlaceTile(position, newTile.GetComponent<Tile>());
+        return newTile;
+    }
+
+    private void PlaceTile(Vector3Int position, Tile tile)
+    {
+        tile.isPlaced = true;
+        if (!tiles.ContainsKey(position))
+        {
+            tiles.Add(position, tile);
+        }
+    }
+
+    public void RemoveTile(Vector3Int position)
+    {
+        // get the cell center in world position
+        Vector3 worldPosition = grid.GetCellCenterWorld(position);
+        tiles.Remove(position);
+    }
+
+    public Vector3 GetNearestGridPoint(Vector3 point)
+    {
+        Vector3Int cellPosition = grid.WorldToCell(point);
+        return grid.GetCellCenterWorld(cellPosition);
+    }
+}
