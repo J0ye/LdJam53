ade654ae6bb0c34e280ea04fa455940e34c03b49
MX-W <wendl.max@gmail.com>
2023-04-30 15:36:45 +0200

    added wall check if tile is placed

--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -7,4 +7,14 @@ public class Tile : MonoBehaviour
 {
     public bool isPlaced { get; set; }
 
+    public List<GameObject> walls;
+
+    private void Start()
+    {
+        walls.ForEach(wall =>
+        {
+            wall.GetComponent<Wall>().tile = this;
+        });
+    }
+
 }
