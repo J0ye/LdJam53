45bd26ca292a30daa336a487265d0d40d1b1604f
Jonathan Müller <jonathan.mueller@fh-erfurt.de>
2023-04-30 20:18:46 +0200

    Added durabillity counter

--- Assets/Script/Tiles/DirectionTile.cs
+++ Assets/Script/Tiles/DirectionTile.cs
@@ -13,11 +13,9 @@ public class DirectionTile : MonoBehaviour
     {
         if (gameObject.GetComponent<Tile>().isPlaced)
         {
-            print("collision");
             Car target;
             if (other.gameObject.TryGetComponent<Car>(out target))
             {
-                print("isCar");
                 // if other has Car component; go here
                 target.SwitchOrientation(direction, true);
             }
